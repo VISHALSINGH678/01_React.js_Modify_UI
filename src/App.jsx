11fd642c053fe8c73080da8d2f8f3bbfc0fef997
Vishal Singh <81406049+VISHALSINGH678@users.noreply.github.com>
2023-12-14 19:16:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css"
+
+export default function App (){
+    let [counter, setCounter] = useState(8)
+
+    const addValue = () =>{
+        setCounter(counter + 1)
+    }
+    const removeValue = () =>{
+        setCounter(counter -1)
+    }
+      
+    return(
+        <div className="A">
+        <h1>Modify UI With Bottons.</h1>
+        <h2>Counter value: <p>{counter}</p></h2>
+        
+        <button onClick={addValue}>+</button>
+        <button onClick={removeValue}>-</button>
+        
+        </div>
+    )
+}
+ App;
